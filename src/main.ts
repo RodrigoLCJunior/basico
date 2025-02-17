bb82e8e87932348238f112cbd6cb32f34ed714ba
Angelozaa <145221675+Angelozaa@users.noreply.github.com>
2025-02-17 13:27:48 -0300

    testezada
    aaaaa

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  let i=1;
+  if(i==0){
+
+  }
 }
 bootstrap();
